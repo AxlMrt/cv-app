0d1e88a9babd0efa45717f9ec720138a0e3a3c16
Axel <eldrank@gmail.com>
2022-11-04 16:44:08 +0100

    feat: Add contact field

--- src/components/CV.js
+++ src/components/CV.js
@@ -4,8 +4,9 @@ import GeneralRender from './cvRender/General';
 import EducationRender from './cvRender/Education';
 import ExperienceRender from './cvRender/Experience';
 import AboutRender from './cvRender/About';
+import ContactRender from './cvRender/Contact';
 
-function CV({ global, education, experience, about }) {
+function CV({ global, education, experience, about, contact }) {
   const { lastName, firstName, job } = global;
 
   return (
@@ -16,28 +17,32 @@ function CV({ global, education, experience, about }) {
           alt="profil pict"
           className="w-full"
         />
-        <div>
-          <h2 className="text-2xl font-bold">Formation</h2>
-          <EducationRender education={education} />
+        <div className='flex flex-col gap-5 text-center'>
+          <h3 className='text-2xl font-bold'>Contact</h3>
+          <ContactRender contact={contact}/>
         </div>
       </div>
-      <div className="h-screen w-full text-zinc-800 border-solid border-2 border-black">
+      <div className="h-screen w-full text-zinc-800 border-2 border-black">
         <GeneralRender
           last={lastName.toUpperCase()}
           first={firstName.toUpperCase()}
           job={job}
         />
 
-        <div>
+        <div className='flex flex-col gap-10 p-12'>
           <div>
-            <h2 className="text-2xl font-bold">A propos</h2>
+            <h2 className="w-full text-2xl border-b-2 border-zinc-800 font-bold uppercase tracking-widest">A propos</h2>
             <AboutRender about={about} />
           </div>
           <div>
-            <h2 className="text-2xl font-bold">
+            <h2 className="text-2xl font-bold uppercase tracking-widest border-b-2 border-zinc-800">
               Experience Professionnelle
             </h2>
             <ExperienceRender experience={experience} />
+          </div>
+          <div>
+            <h2 className="text-2xl font-bold uppercase tracking-widest border-b-2 border-zinc-800">Formation</h2>
+            <EducationRender education={education} />
           </div>
         </div>
       </div>
--- src/components/cvRender/Experience.js
+++ src/components/cvRender/Experience.js
@@ -3,12 +3,20 @@ import React from "react";
 function ExperienceRender({experience}) {
   
   const exp = experience.map((items, idx) => {
+    const start1 = items.start1.substring(0, items.start1.lastIndexOf("-"));
+    const end1 = items.end1.substring(0, items.end1.lastIndexOf("-"));
     return (
-      <div key={idx} className="flex gap-5">
-        <p>{items.enterprise}</p>
-        <p>{items.post}</p>
-        <p>{items.start1}</p>
-        <p>{items.end1}</p>
+      <div key={idx} className="flex gap-5 py-2">
+        <div className="flex gap-5">
+          <p>{start1}</p>
+          <p>{end1}</p>
+        </div>
+        <div>
+          <p className="font-medium text-lg">{items.post}</p>
+          <p>{items.enterprise} à {items.city}</p>
+        </div>
+          
+        
       </div>
     )
   });
